153d93d6edde951c6bab95c97e1fca1d718bd17a
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-07-28 14:13:41 -0600

    Move antlr4 generated files to antlr directory

--- build-syntax.bat
+++ build-syntax.bat
@@ -2,4 +2,4 @@ SET CLASSPATH=.;C:\Javalib\antlr-4.7-complete.jar;%CLASSPATH%
 doskey antlr4=java org.antlr.v4.Tool $*
 doskey grun =java org.antlr.v4.gui.TestRig $*
 
-antlr4 -o syntax JASS.g4 -Dlanguage=JavaScript -visitor
+antlr4 -o antlr JASS.g4 -Dlanguage=JavaScript -visitor
