02844fccfa877fdfc6922a703399d1b19d0d83cd
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-08-03 20:55:46 -0600

    Implement proper index.js file for language support

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-const
-    antlr4 = require('antlr4'),
-    JASSLexer = require('./antlr/JASSLexer'),
-    JASSParser = require('./antlr/JASSParser'),
-    MyJASSListener = require('./test/MyJassListener').MyJASSListener;
-    //c3 = require('antlr4-c3');
+module.exports = {
+    grammar: 'JASS.g4',
 
-const fs = require('fs');
-const http = require('http');
+    /**
+     * @method transpiler
+     * @param {String} code The code in this language
+     * @param {Function} cb Callback function, with error and result
+     *
+     * A method to translate code in this given language
+     * into valid, native JASS, which WC3 will recognize
+     */
+    transpiler: function(code, cb) {
+        // In this case, JASS just maps to itself
+        // since it is already in JASS form
+        return cb(false, code);
+    },
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-    });
-    
-    res.write('<html><head><title>JASS Grammar</title><meta charset="UTF-8"/></head><body>');
-    
-    var input = fs.readFileSync('./test/input.txt', { encoding: 'utf8' });
-    var chars = new antlr4.InputStream(input);
-    var lexer = new JASSLexer.JASSLexer(chars);
-    var tokens  = new antlr4.CommonTokenStream(lexer);
-    var parser = new JASSParser.JASSParser(tokens);
-    parser.buildParseTrees = true;
-    var tree = parser.code();
 
-    var listener = new MyJASSListener(res);
-    antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
-    
-    res.write('<textarea readonly style="width:800px;height:400px;">' + input + '</textarea></body></html>');
-    res.end();
-}).listen(1000);
+    /**
+     * @method pretty
+     * @param {String} code The code in this language
+     * @param {Function} cb Callback function, with error and result
+     *
+     * A method to "pretty print" or auto-format the given input
+     * code. This method DOES NOT MODIFY the behavior of the code;
+     * it only formats the code to follow a certain standard.
+     *
+     * E.g. one might require a space before the ( ) of a function call,
+     * or spaces between commas of an argument list.
+     */
+    pretty: function(code, cb) {
+        // This logic is not yet implemented
+        // TODO: Implement this logic for JASS
+        return cb(false, code);
+    },
 
-console.log('Listening on port: 1000');
 
-// console.log(parser.ruleNames)
-// console.log(parser.symbolicNames)
+    /**
+     * @method validate
+     * @param {String} code The code in this language
+     * @param {Function} cb Callback function, with error flag and array of errors
+     *
+     * A method to verify the validity of the given code in this language.
+     * The first param on the callback is true if there are errors, and false otherwise.
+     * The second param is an array, containing the errors or else it is empty.
+     */
+    validate: function(code, cb) {
+        // TODO: Implement this logic for JASS
+        // e.g. if there are errors with the given code, return cb(true, [...])
+
+        return cb(false, []); // Temporary until the logic is implemented
+    }
+};
